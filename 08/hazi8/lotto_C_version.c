02f2fa3b38cc140eaf4e88bcee2c86a7565a95c3
bala1228 <mikbalazs28@gmail.com>
2021-03-31 10:24:41 +0200

    Debugging in Lotto

--- 08/hazi8/lotto_C_version.c
+++ 08/hazi8/lotto_C_version.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int test_flow(int tomb[], int max, int min, int n)
+int test_flow(int tomb[], int max, int min, int size)
 {
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < size; i++)
     {
         if (tomb[i] > max)
         {
@@ -23,11 +23,11 @@ int test_flow(int tomb[], int max, int min, int n)
 }
 
 // Buborék rendezés
-void array_sorting(int tomb[], int meret)
+void array_sorting(int tomb[], int size)
 {
-    for (int i = 1; i < meret; i++)
+    for (int i = 1; i < size; i++)
     {
-        for (int j = meret - 1; j >= i; j--)
+        for (int j = size - 1; j >= i; j--)
         {
             if (tomb[j - 1] > tomb[j])
             {
@@ -37,16 +37,16 @@ void array_sorting(int tomb[], int meret)
             }   
         }   
     }
-    for (int i = 0; i < meret - 1; i++)
+    for (int i = 0; i < size - 1; i++)
     {
         printf("%d, ", tomb[i]);
     }
-    printf("%d\n", tomb[meret - 1]);
+    printf("%d\n", tomb[size - 1]);
 }
 
 int main()
 {
-    int n = 5;
+    int size = 5;
     int min = 1;
     int max = 90;
     
@@ -62,15 +62,27 @@ int main()
     scanf("%d", &max);
     */
 
-    int test[n];
+    int test[5] = { 1, 2, 3, 3, 4 };
        
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < size; i++)
     {
-        test[i] = (rand() % (max - min + 1) + min);
+        for (int i = 0; i < size - 1; i++)
+        {
+            printf("%d, ", test[i]);
+        }
+        
+        //test[i] = (rand() % (max - min + 1) + min);
+        for (int j = 0; j < size; j++)
+        {
+            if (test[j] == test[i])
+            {
+                test[i] = (rand() % (max - min + 1) + min);
+            }
+        }
     }
     
-    array_sorting(test, n);
-    test_flow(test, max, min, n);
+    array_sorting(test, size);
+    test_flow(test, max, min, size);
     
     
     puts("");
