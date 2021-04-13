105461a16118939986f771fc5772c735e7dd6971
bala1228 <mikbalazs28@gmail.com>
2021-04-13 10:28:04 +0200

    kimenet tartalmaz egyforma elemet, eljárás nem jó

--- 08/hazi8/lotto_C_version.c
+++ 08/hazi8/lotto_C_version.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int test_flow(int tomb[], int max, int min, int size)
 {
@@ -44,11 +45,29 @@ void array_sorting(int tomb[], int size)
     printf("%d\n", tomb[size - 1]);
 }
 
+void array_without_repeat(int array[], int size, int min, int max)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = i + 1; j < size; j++)
+        {
+            if (array[i] == array[j])
+            {
+                array[i] = rand() % (max - min + 1) + min;
+                j--;    
+            }
+        
+            
+        }
+    }
+}
+
 int main()
 {
     int size = 0;
     int min = 0;
     int max = 0;
+    int i, temp;
     
     
     // Bekérjük hány random számot genráljunk.
@@ -62,23 +81,22 @@ int main()
     scanf("%d", &max);
 
     int test[size];
-       
-    for (int i = 0; i < size; i++)
+
+    for (i = 0; i < size; i++)
     {
-        test[i] = (rand() % (max - min + 1) + min);
-
-        for (int j = 1; j < size; j++)
-        {
-            if (test[j] == test[i])
-            {
-                test[i] = (rand() % (max - min + 1) + min);
-            }
-        }
+        test[i] = 0;
     }
+    
+    //srand();
 
+    for (i = 0; i < size; i++)
+    {
+        test[i] = (rand() % (max - min + 1) + min);        
+    }
+    
+    array_without_repeat(test, size, min, max);
     array_sorting(test, size);
     test_flow(test, max, min, size);
-    
     
     puts("");
 
