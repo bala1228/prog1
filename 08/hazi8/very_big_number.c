051a883f3896c791f2416f928c7a76c42bcd0443
bala1228 <mikbalazs28@gmail.com>
2021-04-13 13:57:40 +0200

    lottó kész

--- 08/hazi8/very_big_number.c
+++ 08/hazi8/very_big_number.c
@@ -2,13 +2,14 @@
 #include <string.h>
 #include "prog1.h"
 
+// Stringben található számjegyek összeadása
 void sum_digits(char array[])
 {
     int length = strlen(array);
     int counter = 0;
     for (size_t i = 0; i < length; i++)
     {
-        counter = counter +  (array[i] - '0');
+        counter = counter + (array[i] - '0');
     }
     
     printf("A számjegyek összege: %d\n", counter);
