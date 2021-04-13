62b9d73484a7f092392e3804fb31e7c5f018c64a
bala1228 <mikbalazs28@gmail.com>
2021-04-13 11:25:32 +0200

    Műkődik megfelelően, kommentek hozzáadása, refactoring

--- 08/hazi8/password.c
+++ 08/hazi8/password.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #define SIZE 100
 
-void password_checker(char password[])
+int password_checker(char password[])
 {
     int lower_case_counter = 0;
     int upper_case_counter = 0;
@@ -12,43 +13,54 @@ void password_checker(char password[])
     if (length < 8)
     {
         printf("gyenge jelszó\n");
+        return 0;
     }
-    for (int i = 0; i < length; i++) 
+    else   
     {
-        if (password[i] >= 'a' && password[i] <= 'z') 
+        for (int i = 0; i < length; i++) 
         {
-            ++lower_case_counter;
-        }
-        if (password[i] >= 'A' && password[i] <= 'Z') 
+            if (password[i] >= 'a' && password[i] <= 'z') 
+            {
+                ++lower_case_counter;
+            }
+            if (password[i] >= 'A' && password[i] <= 'Z') 
+            {
+                ++upper_case_counter;
+            }
+            if (password[i] >= '0' && password[i] <= '9')
+            { 
+                ++digit_counter;
+            } 
+        }    
+        if (lower_case_counter > 0 && upper_case_counter > 0 && digit_counter > 0) 
         {
-        ++upper_case_counter;
+            printf("erős jelszó\n");
+            return 1;
+        }   
+        else
+        {
+            printf("gyenge jelszó\n");
+            return 0;
         }
-        if (password[i] >= '0' && password[i] <= '9')
-        { 
-            ++digit_counter;
-        } 
-    }    
-    if (lower_case_counter > 0 && upper_case_counter > 0 && digit_counter > 0) 
-    {
-        printf("erős jelszó\n");
-    }
-    else
-    {
-        printf("gyenge jelszó\n");
     }
 }
 
 int main()
 {
     char password[SIZE];
-    
-    do
+
+    while (1)
     {
         printf("Enter a password: ");
         scanf("%s", &password);
 
+        if (strchr(password, '*') != NULL)
+        {
+            exit(0);
+        }
+        
         password_checker(password);
-    } while (password != "*");
+    }
     
     return 0;
 }    
