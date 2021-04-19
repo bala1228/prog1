c0ffd54ae83297a48c70af8b0143a2f6faeba5e4
bala1228 <mikbalazs28@gmail.com>
2021-04-19 13:57:35 +0200

    Program fut. Hibás bemenetet lekezeli.

---
+++ 09/hazi9/shortest_word.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[])
+{
+    // Ha nincs megadva parancsori argumentumként egy szó sem, 
+    // akkor hibaüzenettel kilép a program.
+    if (argc < 2)
+    {
+        printf("Nem adtál meg egyetlen szót sem!\n");
+        exit(1);
+    }
+    // Összeadja a 2 paramétert.
+    else
+    {   
+        
+    }
+    exit(0);
+}
