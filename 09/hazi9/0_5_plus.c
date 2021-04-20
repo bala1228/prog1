c5787bd2b8e516a3e0179a62d6e12c3bbdab38ca
bala1228 <mikbalazs28@gmail.com>
2021-04-20 17:45:28 +0200

    Program kész. Linux tesztelés kell.

--- 09/hazi9/0_5_plus.c
+++ 09/hazi9/0_5_plus.c
@@ -4,21 +4,20 @@
 
 int main()
 {
-    FILE *fp;
-    char filename[SIZE] = "out.txt";
-    char filename_2[SIZE] = "out_2.txt";
+    FILE *fp, *fp_2;
+    char filename[SIZE] = "in.txt";
+    char filename_2[SIZE] = "out.txt";
     char buffer[SIZE];
     int count = 0;
     char c;
 
     fp = fopen(filename, "r");
     printf("# in.txt sikeresen megnyitva\n");
-    FILE *fp_2 = fopen(filename_2, "w");
+    fp_2 = fopen(filename_2, "w");
     
     printf("# 0,5-nél nagyobb számok szűrése...\n");
     
     
-
     while(fgets(buffer, SIZE, fp)) 
     {
         if (atof(buffer) > 0.5)
@@ -27,11 +26,14 @@ int main()
         }  
     }
     
-    for (c = getc(fp); c != EOF; c = getc(fp))
+    fclose(fp_2);
+    fp_2 = fopen(filename_2, "r");
+    
+    for (c = getc(fp_2); c != EOF; c = getc(fp_2))
     {    
         if (c == '\n')
         {    
-            count = count + 1;
+            count++;
         }
     }
     
