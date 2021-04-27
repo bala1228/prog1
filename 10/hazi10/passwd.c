052a8008f8278b44e28154061a6ef787e02a5b26
bala1228 <mikbalazs28@gmail.com>
2021-04-27 19:50:38 +0200

    kész

---
+++ 10/hazi10/passwd.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+  
+// legalább 8, max 12 karakter hosszú
+// tartalmaz kis betűt
+// tartalmaz nagy betút
+// tartalmaz számjegyet
+// tartalmaz speciális karaktert
+void passwordgenerator()
+{
+    srand((time(NULL)));
+
+    // Hossz generálása (min 8, max 12)
+    int length = 0;
+    while (length < 8)
+    {
+        length = rand() % 13;
+    }  
+  
+    // Generálás forrásai
+    char *numbers = "0123456789";
+  
+    char *letter = "abcdefghijklmnoqprstuvwyzx";
+  
+    char *LETTER = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
+  
+    char *symbols = "!@#$^&*?";
+  
+    char password[length];
+  
+    
+    int random = rand() % 4;
+  
+    for (int i = 0; i < length; i++) {
+  
+        if (random == 1) {
+            password[i] = numbers[rand() % 10];
+            random = rand() % 4;
+            printf("%c", password[i]);
+        }
+        else if (random == 2) {
+            password[i] = symbols[rand() % 8];
+            random = rand() % 4;
+            printf("%c", password[i]);
+        }
+        else if (random == 3) {
+            password[i] = LETTER[rand() % 26];
+            random = rand() % 4;
+            printf("%c", password[i]);
+        }
+        else {
+            password[i] = letter[rand() % 26];
+            random = rand() % 4;
+            printf("%c", password[i]);
+        }
+    }
+}
+  
+int main()
+{
+    passwordgenerator();
+  
+    exit(0);
+}
---
+++ 10/hazi10/random_item.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+
+int choice(const int n, const int tomb[])
+{
+    srand((time(NULL)));
+    int index = rand() % n;
+
+    return tomb[index];
+}
+
+int main()
+{   
+    int array[10] = { 0 };
+    int array_size = 10;
+    for (int i = array_size; i != 0; i--)
+    {
+        array[i] = rand() % 1024;
+    }
+    for (int i = array_size; i != 0; i--)
+    {
+        printf("%d ", array[i]);
+    }
+    puts("");
+    
+    printf("%d\n", choice(array_size, array));
+
+    return 0;
+}
