8ce81d8e9815775822521634bb19cfcac139b895
bala1228 <mikbalazs28@gmail.com>
2021-05-01 14:05:28 +0200

    kész.

--- 11/hazi11/strcmpi.c
+++ 11/hazi11/strcmpi.c
@@ -3,41 +3,48 @@
 #include <string.h>
 #include <ctype.h>
 
-void string_to_upper(char *s)
+void string_to_upper(char s[])
 {
-    while (*s) 
+    int length = strlen(s);
+    for (int i = 0; i < length; i++)
     {
-        *s = toupper(*s);
-        s++;
+        s[i] = toupper(s[i]);
     }
-}
+}    
 
 int my_strcmpi(const char *s1, const char *s2)
 {   
-    const unsigned char *p1 = (const unsigned char *) s1;
-    const unsigned char *p2 = (const unsigned char *) s2;
-    unsigned char c1, c2;
+    char *temp_1 = strdup(s1);
+    char *temp_2 = strdup(s2);
+    
+    string_to_upper(temp_1);
+    string_to_upper(temp_2);
+
+    char const *p1 = temp_1;
+    char const *p2 = temp_2;
+    char c1, c2;
     do
     {
-        c1 = (unsigned char) *s1++;
-        c2 = (unsigned char) *s2++;
+        c1 = *p1++;
+        c2 = *p2++;
         if (c1 == '\0')
         {
-            return c1 - c2;
+            break;
         }
-    }
-    while (c1 == c2);
-    
+    } while (c1 == c2);
+
+    free(temp_1);
+    free(temp_2);
+
     return c1 - c2;
 }
 
 int main()
 {
-    char *s = "ez";
-    char *string_2 = "Ez";
-    string_to_upper(s);
+    char s1[] = "AlAdAr";
+    char s2[] = "aLadAR";
     
-    printf("%s\n", s);
+    printf("%d\n", my_strcmpi(s1, s2));
     
     return 0;
 }
