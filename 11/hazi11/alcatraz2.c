1eef0b70adf70dc9ed23fabce9a7dad76a6a62ef
bala1228 <mikbalazs28@gmail.com>
2021-05-03 13:30:55 +0200

    proba

---
+++ 11/hazi11/alcatraz2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+ 
+int main(int argc, char const *argv[])
+{
+    int* cella = (int*)calloc(600,sizeof(int));
+    for(int i=1;i<=600;i++){
+        for(int j=1;j<=600;j++){
+            if(j % i == 0){
+                cella[i-1] = 1;
+            }
+            else{
+                cella[i-1] = 0;
+            }
+        }
+    }
+    for(int i=1;i<=600;i++){
+        printf("index %d : %d\n",i,cella[i-1]);
+    }
+    return 0;
+}
