ddfa95376c3485dda600fbcfa95648a889f800ac
bala1228 <mikbalazs28@gmail.com>
2021-05-04 14:36:04 +0200

    kész

--- 11/hazi11/alcatraz.c
+++ 11/hazi11/alcatraz.c
@@ -4,30 +4,28 @@
 int main()
 {
     int* cella = (int*)calloc(600, sizeof(int));
+
     for (int i = 1; i <= 600; i++)
-    {
+    {   
         for (int j = 1; j <= 600; j++)
         {
             if(j % i == 0)
             {
-                cella[i - 1] = 1;
-            }
-            else
-            {
-                cella[i - 1] = 0;
+                cella[j - 1] = 1 - cella[j - 1];
             }
         }
     }
     for (int i = 1; i <= 600; i++)
     {
-        printf("index %d : %d\n", i, cella[i - 1]);
-        /*
+        //printf("index %d : %d\n", i, cella[i - 1]);
+        
         if (cella[i - 1] == 1)
         {
             printf("%d", i);
         }
-        */
+        
     }
+    puts("");
 
     free(cella);
 
